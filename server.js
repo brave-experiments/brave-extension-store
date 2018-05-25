cb2528505e2b87dab276ad896133863ff6d2107f
Ailin Liao <ailinliao@ailins-mbp.attlocal.net>
2018-05-25 14:16:14 -0700

    Initial - hapi.js setup and chromium extension.html

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Hapi=require('hapi');
+
+// Create a server with a host and port
+const server=Hapi.server({
+    host:'localhost',
+    port:8000
+});
+
+// Add the route
+server.route({
+    method:'GET',
+    path:'/extension-store',
+    handler:function(request,h) {
+        return request.params.extensions_mock.html;
+    }
+});
+
+// Start the server
+async function start() {
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
