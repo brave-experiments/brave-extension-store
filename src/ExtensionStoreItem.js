369e57b3b0e79cb2c7018755182f2838c9524ae8
Brian Clifton <brian@clifton.me>
2018-06-18 13:59:32 -0700

    Add some basic styling - Extension item is now it's own control - Extension store list loads automatically - Spinner is shown when AJAX calls are made (added file-loader for webpack) - Extension store item button is ready to be wired up - Removed old/stale `store.html` file

--- src/ExtensionStoreItem.js
+++ src/ExtensionStoreItem.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const React = require('react')
 const { PushButton } = require('brave-ui')
@@ -9,12 +9,29 @@ class ExtensionStoreItem extends React.Component {
     this.state = {
       extensions: []
     }
+
+    this.install = this.install.bind(this)
+  }
+
+  install () {
+    console.log('install for ' + this.props.name + ' clicked')
   }
 
   render () {
-    return <div>
-      <div>{this.props.id}</div>
-      <PushButton>Install me</PushButton>
+    const rootStyle = {
+      display: 'inline-block',
+      marginTop: '10px',
+      marginRight: '10px',
+      border: '1px solid black',
+      minWidth: '150px',
+      minHeight: '60px',
+      textAlign: 'center'
+    }
+
+    return <div style={rootStyle}>
+      <div>{this.props.name}</div>
+      <div>Version {this.props.version}</div>
+      <PushButton onClick={this.install}>Install</PushButton>
     </div>
   }
 }
--- src/ExtensionStoreList.js
+++ src/ExtensionStoreList.js
@@ -1,16 +1,22 @@
-'use strict';
+'use strict'
 
 const React = require('react')
-const { PushButton } = require('brave-ui')
+const { TitleHeading, PushButton } = require('brave-ui')
 const ExtensionStoreItem = require('./ExtensionStoreItem')
+const spinnerImage = require('./i/ajax-loader.gif')
 
 class ExtensionStoreList extends React.Component {
   constructor (props) {
     super()
     this.state = {
+      loading: false,
       extensions: []
     }
     this.fetchExtensions = this.fetchExtensions.bind(this)
+  }
+
+  componentDidMount () {
+    this.fetchExtensions()
   }
 
   fetchExtensions () {
@@ -18,23 +24,25 @@ class ExtensionStoreList extends React.Component {
     const self = this
 
     xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-           if (xmlhttp.status == 200) {
-              const response = JSON.parse(xmlhttp.response)
-              const extensions = []
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+        if (xmlhttp.status == 200) {
+          const response = JSON.parse(xmlhttp.response)
+          const extensions = []
 
-              response.extensions.forEach((extension) => {
-                // console.log('TEST: extension', extension)
-                extensions.push(extension)
-              })
+          response.extensions.forEach((extension) => {
+            // console.log('TEST: extension', extension)
+            extensions.push(extension)
+          })
 
-              self.setState({extensions})
-           } else {
-              console.log('Request returned ' + xmlhttp.status)
-           }
+          self.setState({extensions, loading: false})
+        } else {
+          console.log('Request returned ' + xmlhttp.status)
+          self.setState({loading: false})
         }
+      }
     }
 
+    this.setState({loading: true})
     xmlhttp.open('GET', '/brave-extension-store', true)
     xmlhttp.send()
   }
@@ -43,45 +51,71 @@ class ExtensionStoreList extends React.Component {
     var xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-           if (xmlhttp.status == 200) {
-              document.getElementById("messages").innerHTML = xmlhttp.response
-
-              // TODO: this.setState()
-              // ex: this.setState('extensions', xmlhtml)
-
-           } else {
-              console.log('Request returned ' + xmlhttp.status)
-           }
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+        if (xmlhttp.status == 200) {
+          document.getElementById("messages").innerHTML = xmlhttp.response
+          // TODO: this.setState()
+          // ex: this.setState('extensions', xmlhtml)
+          self.setState({loading: false})
+        } else {
+          console.log('Request returned ' + xmlhttp.status)
+          self.setState({loading: false})
         }
+      }
     }
 
+    this.setState({loading: true})
     xmlhttp.open('GET', '/brave-extension-store-update', true)
     xmlhttp.send()
   }
 
   render () {
-    return <div>
-      <h1>Extensions</h1>
+    const rootStyle = {
+      backgroundColor: '#f1f1f1',
+      width: '1200px',
+      marginTop: '10px',
+      display: 'inline-block',
+      textAlign: 'left',
+      borderLeft: '1px solid black',
+      borderRight: '1px solid black',
+      padding: '5px'
+    }
+    const bottomStyle = {
+      marginTop: '10px'
+    }
+
+    return <div style={rootStyle}>
+      <TitleHeading
+        text='Brave Extension Store'
+        label='beta'
+        />
+
       <div id="extensions">
-        {
-          this.state.extensions.map((extension) => {
-            return <ExtensionStoreItem
-              id={extension[0]}
-              />
-          })
-        }
+      {
+        this.state.loading
+          ? <div><img src={spinnerImage} /> Loading...</div>
+          : this.state.extensions.map((extension) => {
+              return <ExtensionStoreItem
+                id={extension[0]}
+                version={extension[1]}
+                sha={extension[2]}
+                name={extension[3]}
+                />
+            })
+      }
       </div>
 
-      <PushButton theme='primary' label='Update Extensions' onClick={this.fetchExtensions}>
-        fetch extensions
-      </PushButton>
+      <div style={bottomStyle}>
+        <PushButton theme='primary' label='Update Extensions' onClick={this.fetchExtensions}>
+          Refresh
+        </PushButton>
 
-      <PushButton theme='primary' label='Update Extensions' onClick={this.checkForUpdates}>
-        check for update
-      </PushButton>
+        <PushButton theme='primary' label='Update Extensions' onClick={this.checkForUpdates}>
+          Check for updates
+        </PushButton>
 
-      <div id="messages"></div>
+        <div id="messages"></div>
+      </div>
     </div>
   }
 }
